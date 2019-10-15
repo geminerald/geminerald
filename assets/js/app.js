3e01a85c97972209dcd28bc963aa30356fd7b589
geminerald <zacdwarner@gmail.com>
2019-10-15 11:27:47 +0100

    added basic js functions to switch between sections on click

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,37 @@
+// Button Selectors
+
 const aboutLink = document.getElementById('about-link');
+const downloadLink = document.getElementById('download-link');
+const portfolioLink = document.getElementById('portfolio-link');
+const contactLink = document.getElementById('contact-link');
+
+// Section Selectors
+
 const homeSection = document.getElementById('home');
 const aboutSection = document.getElementById('about');
+const portfolioSection = document.getElementById('portfolio');
+const contactSection = document.getElementById('contact');
+
+// Nav Button listeners
 
 aboutLink.addEventListener('click',()=>{
-    homeSection.classList.add('to-back');
+
     aboutSection.classList.add('to-front');
+
+    homeSection.classList.remove('to-front');
+    portfolioSection.classList.reomve('to-front');
+});
+
+portfolioLink.addEventListener('click', ()=>{
+
+    portfolioSection.classList.add('to-front');
+
+    homeSection.classList.remove('to-front');
+    aboutSection.classList.remove('to-front');
+});
+
+contactLink.addEventListener('click',()=>{
+    contactSection.classList.add('to-front');
+    homeSection.classList.remove('to-front');
+    portfolioSection.classList.remove('to-front');
 });
