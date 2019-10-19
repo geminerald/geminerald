3e29ae9279d0ce72684f74864f053d64603e9885
geminerald <zacdwarner@gmail.com>
2019-10-19 17:17:52 +0100

    fixed bug where name was not appearing in email

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,15 +1,25 @@
-function sendMail(contactForm){
-    emailjs.send("gmail","zac_cv",{
-        "from_name":contactForm.name.value,
-        "from_email":contactForm.emailaddress.value,
-        "project_request":contactForm.projectsummary.value
-    })
-    .then(
-        function(response){
-            console.log("success",response);
-        },
-        function(error){
-            console.log("failed", error);
-        }
-    )
+function sendMail(contactForm) {
+    emailjs.send("gmail", "zac_cv", {
+            "from_name": contactForm.fullname.value,
+            "from_email": contactForm.emailaddress.value,
+            "project_request": contactForm.projectsummary.value
+        })
+        .then(
+            function (response) {
+                console.log("success", response);
+            },
+            function (error) {
+                console.log("failed", error);
+            }
+        );
+    return false; // To block from loading a new page
 };
+
+function showModal(result){
+    if(result == success){
+        console.log("success modal can go here");
+    }
+    if (result == error){
+        console.log("failed modal can go here, directing user to the email address or similar. ")
+    }
+}
