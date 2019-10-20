1379e7cb368d1d5d7326351c6146428fc8edc147
geminerald <zacdwarner@gmail.com>
2019-10-20 10:51:28 +0100

    added links to page sections and smooth scroll. Removed About HTML page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,39 +1,39 @@
-// Nav Button Selectors
+// // Nav Button Selectors
 
-const aboutLink = document.getElementById('about-link');
-const downloadLink = document.getElementById('download-link');
-const portfolioLink = document.getElementById('portfolio-link');
-const contactLink = document.getElementById('contact-link');
+// const aboutLink = document.getElementById('about-link');
+// const downloadLink = document.getElementById('download-link');
+// const portfolioLink = document.getElementById('portfolio-link');
+// const contactLink = document.getElementById('contact-link');
 
-// Section Selectors
+// // Section Selectors
 
-const homeSection = document.getElementById('home');
-const aboutSection = document.getElementById('about');
-const portfolioSection = document.getElementById('portfolio');
-const contactSection = document.getElementById('contact');
+// const homeSection = document.getElementById('home');
+// const aboutSection = document.getElementById('about');
+// const portfolioSection = document.getElementById('portfolio');
+// const contactSection = document.getElementById('contact');
 
-// Nav Button listeners
+// // Nav Button listeners
 
-aboutLink.addEventListener('click', () => {
-    aboutSection.classList.add('to-front');
-    homeSection.classList.remove('to-front');
-    portfolioSection.classList.remove('to-front');
-    contactSection.classList.remove('to-front');
-});
+// aboutLink.addEventListener('click', () => {
+//     aboutSection.classList.add('to-front');
+//     homeSection.classList.remove('to-front');
+//     portfolioSection.classList.remove('to-front');
+//     contactSection.classList.remove('to-front');
+// });
 
-portfolioLink.addEventListener('click', () => {
-    portfolioSection.classList.add('to-front');
-    homeSection.classList.remove('to-front');
-    aboutSection.classList.remove('to-front');
-    contactSection.classList.remove('to-front');
-});
+// portfolioLink.addEventListener('click', () => {
+//     portfolioSection.classList.add('to-front');
+//     homeSection.classList.remove('to-front');
+//     aboutSection.classList.remove('to-front');
+//     contactSection.classList.remove('to-front');
+// });
 
-contactLink.addEventListener('click', () => {
-    contactSection.classList.add('to-front');
-    homeSection.classList.remove('to-front');
-    portfolioSection.classList.remove('to-front');
-    aboutSection.classList.remove('to-front');
-});
+// contactLink.addEventListener('click', () => {
+//     contactSection.classList.add('to-front');
+//     homeSection.classList.remove('to-front');
+//     portfolioSection.classList.remove('to-front');
+//     aboutSection.classList.remove('to-front');
+// });
 
 
 // Preview buttons on portfolio cards
